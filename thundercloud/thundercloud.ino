2cde3bb196fd1fe753aff129ebc8aa41079a9bfb
Marc Grützmacher <me@whoismarc.info>
2018-01-07 22:44:02 +0100

    Refactor current state

--- thundercloud/thundercloud.ino
+++ thundercloud/thundercloud.ino
@@ -12,28 +12,29 @@ boolean BUTTON_INPUT = 0;
 // ANALOG VALUES
 int MICRO_INPUT = 0;
 
+// MICROPHONE
+int INPUT_MAX = 0;
+int INPUT_MIN = 1024;
+double THRESHOLD = 0.8; // do not set below 0.5 because of noise floor
+
+// PIXEL STRIPES
+int NUM_LEDS = 75;
+
 // MISC
 int CLOUD_MODE = 1;   // 0 = All on; 1 = Thunder blue; 2 = Thunder orange; 3 = Party
-const int NUM_LEDS = 75;
-const int CLOUD_MODE_MAX = 4;
-const double MICRO_THRESHOLD_MIN = 0.30;
-const double MICRO_THRESHOLD_MAX = 0.70;
-const double LED_INTENSITY_MIN = 0.30;
-const double LED_INTENSITY_MAX = 1.00;
-const int sampleWindow = 50;  // Sample window width in mS (50 mS = 20Hz)
+int CLOUD_MODE_MAX = 4;
+int COUNTER = 0; // Counter for decreasing max value
+int DECREASE_CHECK = 60000; // Time to check decrease input max value
+int LAST_STRIKE = 0;
+boolean BUTTON_PRESSED = false;
+int BUTTON_RELEASE = 500; // Time to release button before next switch
 
-// Create neopixel stripe
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-void setup() {
+// -------------------------------------------------
 
+void setup() {
   // Neopixel setup
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
@@ -48,121 +49,125 @@ void setup() {
   digitalWrite(BUTTON_5V_PIN, true);
 }
 
+// -------------------------------------------------
+
 void loop() {
+  int microSignal = analogRead(MICRO_PIN);
+  boolean buttonInput = digitalRead(BUTTON_PIN);
 
-  // -----------------------------------------------
-  // Toggle cloud mode + reset led strip
-  BUTTON_INPUT = digitalRead(BUTTON_PIN);
-  if (BUTTON_INPUT) {
-    turnAllPixelsOff();
-    CLOUD_MODE++;
+  COUNTER++;
 
-    if (CLOUD_MODE >= CLOUD_MODE_MAX) {
-      CLOUD_MODE = 0;
-    }
-    delay(500);
+  // Decrease input max to normalize the max peaks if no light flashes
+  // checkForInputMaxDecrease();
+
+  // Check for a button press
+  if (buttonInput && BUTTON_PRESSED == false) {
+    changeMode();
   }
 
-  // -----------------------------------------------
-  unsigned long startMillis = millis(); // Start of sample window
-  unsigned int peakToPeak = 0;   // peak-to-peak level
-  double intensity = 0;
+  // Let's do some magic!
+  run(getIntensity(microSignal));
 
-  unsigned int signalMax = 0;
-  unsigned int signalMin = 1024;
-
-  // collect data for 50 mS
-  while (millis() - startMillis < sampleWindow) {
-    MICRO_INPUT = analogRead(MICRO_PIN);
-    if (MICRO_INPUT < 1024) {  // toss out spurious readings
-      if (MICRO_INPUT > signalMax) {
-        signalMax = MICRO_INPUT;  // save just the max levels
-      }
-      else if (MICRO_INPUT < signalMin) {
-        signalMin = MICRO_INPUT;  // save just the min levels
-      }
-    }
-  }
-  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-  intensity = ((peakToPeak * 5.0) / 1024);  // convert to volts
-
-   // -----------------------------------------------
-
-  getItOn(intensity);
-}
-
- // -----------------------------------------------
-
-void getItOn(double intensity){
-
-  // -----------------------------------------------
-  // ALL ON
-  if (CLOUD_MODE == 0) {
-    turnAllPixelsOn();
-  }
-
-  // -----------------------------------------------
-  // THUNDER BLUE = 1 / ORANGE = 2
-  if (CLOUD_MODE == 1 || CLOUD_MODE == 2) {
-      Serial.print(intensity, 3);
-      Serial.print(" - ");
-      Serial.print(intensity > MICRO_THRESHOLD_MIN);
-      Serial.print(" - ");
-      Serial.println(MICRO_THRESHOLD_MIN);
-
-      if (intensity > MICRO_THRESHOLD_MIN) {
-        int led = random(NUM_LEDS);
-
-        for (int i = 0; i < 10; i++) {
-          lightningStrike(random(NUM_LEDS), intensity);
-        }
-      }
-  }
-
-  // -----------------------------------------------
-  // PARTY MODE
-  if (CLOUD_MODE == 3) {
-    lightningStrike(random(NUM_LEDS), random(0, 1));
-  }
-
-  delay(150);
+  delay(1); // delay in between reads for stability
 }
 
 // -------------------------------------------------
 
-void lightningStrike(int pixel, double intensity) {
-
-  int rColor = 255;
-  int gColor = 255;
-  int bColor = 255;
-
-  if (CLOUD_MODE == 2) {
-    rColor = 255;
-    gColor = 69;
-    bColor = 0;
+void run(double intensity) {
+  
+  // All pixels on
+  if (CLOUD_MODE == 0) {
+    turnAllPixelsOn();
   }
 
-  if (CLOUD_MODE == 3) {
-    rColor = random(0, 255);
-    gColor = random(0, 255);
-    bColor = random(0, 255);
+  // Thunder blue mode
+  else if (CLOUD_MODE == 1) {
+    if (intensity >= THRESHOLD) {
+      int color[3] = {255, 255, 255};
+      lightningStrike(random(NUM_LEDS), intensity, color);
+    }
   }
 
-  double tmpMax = MICRO_THRESHOLD_MAX - MICRO_THRESHOLD_MIN;
-  double tmpCur = intensity - MICRO_THRESHOLD_MIN;
-  double percent = 0 / tmpMax;
-  double tmpLedMax = LED_INTENSITY_MAX - LED_INTENSITY_MIN;
-  double newLedIntensity = (tmpLedMax * percent) + LED_INTENSITY_MIN;
+  // Thunder purple mode
+  else if (CLOUD_MODE == 2) {
+    if (intensity > THRESHOLD) {
+      int color[3] = {255, 69, 0};
+      lightningStrike(random(NUM_LEDS), intensity, color);
+    }
+  }
 
-  int newRColor = (int)(rColor * newLedIntensity);
-  int newGColor = (int)(gColor * newLedIntensity);
-  int newBColor = (int)(bColor * newLedIntensity);
+  // Party mode
+  else if (CLOUD_MODE == 3) {
+    int color[3] = {random(0, 255), random(0, 255), random(0, 255)};
+    double randomIntensity = random(0, 100) / 100.0;
+    lightningStrike(random(NUM_LEDS), randomIntensity, color);
+  }
+}
 
-  strip.setPixelColor(pixel, strip.Color(newRColor, newGColor, newBColor));
+// -------------------------------------------------
+
+void lightningStrike(int pixel, double intensity, int color[]) {
+  LAST_STRIKE = COUNTER;
+  
+  // create color from intensity
+  int rColor = (int)(color[0] * intensity);
+  int gColor = (int)(color[1] * intensity);
+  int bColor = (int)(color[2] * intensity);
+
+  // light pixel
+  strip.setPixelColor(pixel, strip.Color(rColor, gColor, bColor));
   strip.show();
-  // delay(random(100, 1000));
+
+  // let it glow for some time
+  delay(random(0, 300));
 
   turnAllPixelsOff();
+}
+
+// -------------------------------------------------
+
+double getIntensity(int microSignal) {
+  double peakToPeak = 0;
+  double peakToPeakPercent = 0; // peak-to-peak current in percent 0-1
+
+  if (microSignal > INPUT_MAX) {
+    INPUT_MAX = microSignal;  // save just the max levels
+  }
+  else if (microSignal < INPUT_MIN) {
+    INPUT_MIN = microSignal;  // save just the min levels
+  }
+
+  peakToPeak = INPUT_MAX - INPUT_MIN;  // max - min = peak-peak amplitude
+  peakToPeakPercent = (microSignal - INPUT_MIN) / peakToPeak;
+
+  return peakToPeakPercent;
+}
+
+// -------------------------------------------------
+
+void changeMode() {
+  BUTTON_PRESSED = true;
+  CLOUD_MODE++;
+
+  if (CLOUD_MODE >= CLOUD_MODE_MAX) {
+    CLOUD_MODE = 0;
+  }
+
+  turnAllPixelsOff();
+  
+  delay(BUTTON_RELEASE);
+
+  BUTTON_PRESSED = false;
+}
+
+// -------------------------------------------------
+
+
+void checkForInputMaxDecrease() {
+  // Decrease input max for one fourth if last strike is more than DECREASE_CHECK away
+  if (COUNTER % DECREASE_CHECK == 0 && COUNTER - DECREASE_CHECK < LAST_STRIKE && INPUT_MAX > 0) {
+    INPUT_MAX = INPUT_MAX - abs(INPUT_MAX / 4);
+  }
 }
 
 // -------------------------------------------------
@@ -182,4 +187,3 @@ void turnAllPixelsOff() {
   }
   strip.show();
 }
-
