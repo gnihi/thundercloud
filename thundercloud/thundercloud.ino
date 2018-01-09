f74fcfb23334d72068f41c76b57db5cbb561583b
Marc Grützmacher <me@whoismarc.info>
2018-01-09 21:30:10 +0100

    Fix bug - max average has to be a double

--- thundercloud/thundercloud.ino
+++ thundercloud/thundercloud.ino
@@ -11,7 +11,7 @@ double THRESHOLD = 0.8;                 // Do not set below 0.5 because of noise
 const int INPUT_READINGS = 10;          // Number of reading to build an average - Has to be a constant!
 int INPUT_MAX[INPUT_READINGS];          // The readings from the analog input
 int INPUT_MAX_INDEX = -1;               // The index of the current reading, -1 because is incremented before first read
-int INPUT_MAX_AVERAGE = 0;              // The average max input level
+double INPUT_MAX_AVERAGE = 0;           // The average max input level
 int INPUT_MIN = 1024;
 
 // MISC
@@ -19,11 +19,19 @@ int CLOUD_MODE = 1;                     // 0 = All on; 1 = Thunder blue; 2 = Thu
 int CLOUD_MODE_MAX = 4;                 // Max number of modes
 int NUM_LEDS = 75;                      // Number of LEDs on the pixel stripe
 int COUNTER = 0;                        // Counter for decreasing max value
-int INPUT_MAX_NORMALIZE_CHECK = 10000;  // Time to check decrease input max value
+int INPUT_MAX_NORMALIZE_CHECK = 100;    // Time to check decrease input max value
 int LAST_STRIKE = 0;                    // Time of last lightning strike
 int BUTTON_RELEASE = 500;               // Time to release button before next possible activation
 boolean BUTTON_PRESSED = false;         // Bool to prevent multiple button activations
 
+// Create neopixel stripe
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 // -------------------------------------------------
@@ -157,6 +165,11 @@ double getIntensity(int microSignal) {
     INPUT_MIN = microSignal;  // save just the min levels
   }
 
+  // Slow down the micro signal on its peak. The high values is includes in the average max readings for the next calculation.
+  if(microSignal > INPUT_MAX_AVERAGE){
+    microSignal = INPUT_MAX_AVERAGE;
+  }
+
   peakToPeak = INPUT_MAX_AVERAGE - INPUT_MIN;  // max - min = peak-peak amplitude
   peakToPeakPercent = (microSignal - INPUT_MIN) / peakToPeak;
 
@@ -193,7 +206,7 @@ void normalizeInputMaxAverage() {
   }
 
   // Decline max peak to average
-  INPUT_MAX[maxReadingIndex] = INPUT_MAX_AVERAGE;  
+  INPUT_MAX[maxReadingIndex] = INPUT_MAX_AVERAGE;
 }
 
 // -------------------------------------------------
